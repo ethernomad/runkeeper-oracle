67f62918df5bdf69481b554eddc0aca2e1f7c775
Jonathan Brown <jbrown@bluedroplet.com>
2016-01-29 17:36:47 +0700

    Add contract.

---
+++ runkeeper.sol
@@ -0,0 +1,52 @@
+contract RunKeeper {
+
+    struct Commitment {
+        uint factId;
+        bytes32 factHash;
+        uint amount;
+        address owner;
+        address defaultAccount;
+        address oracle;
+        uint threshold;
+    }
+
+    mapping (bytes32 => Commitment) commitments;
+    mapping (address => bytes32[]) accountCommitments;
+
+    function makeCommitment(uint factId, bytes32 factHash, address defaultAccount, address oracle, uint threshold) external returns (bytes32 hash) {
+        hash = sha3(this, msg.sender, msg.value, msg.data);
+        commitments[hash] = Commitment({
+            factId: factId,
+            factHash: factHash,
+            amount: msg.value,
+            owner: msg.sender,
+            defaultAccount: defaultAccount,
+            oracle: oracle,
+            threshold: threshold,
+        });
+        accountCommitments[msg.sender].push(hash);
+    }
+
+    function settle(bytes32 hash, bytes32 resultHex, uint8 v, bytes32 r, bytes32 s) external returns (bool) {
+        Commitment commitment = commitments[hash];
+
+        bytes32 result_hash = sha3(commitment.factHash, resultHex);
+        address signer_address = ecrecover(result_hash, v, r, s);
+
+        if (signer_address != commitment.oracle) {
+            throw;
+        }
+
+        uint result = uint(resultHex);
+
+        if (result > commitment.threshold) {
+            commitment.owner.send(commitment.amount);
+            return true;
+        }
+        else {
+            commitment.defaultAccount.send(commitment.amount);
+            return false;
+        }
+    }
+
+}
